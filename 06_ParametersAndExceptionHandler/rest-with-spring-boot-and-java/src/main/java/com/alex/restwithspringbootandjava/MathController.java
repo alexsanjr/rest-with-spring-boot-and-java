831fb46d681e1a682840de19afc700f3154965a4
Alex Sandro <alex.sandro.1199@gmail.com>
2023-03-01 18:57:07 -0300

    sum created

--- 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/MathController.java
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/MathController.java
@@ -7,12 +7,30 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class MathController {
 
-    private static final AtomicLong counter = new AtomicLong();
+	private static final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/sum/{num1}/{num2}")
-    public Double sum(
-            @PathVariable(value = "num1") String num1,
-            @PathVariable(value = "num2") String num2) {
-        return 2D;
-    }
+	@GetMapping("/sum/{num1}/{num2}")
+	public Double sum(@PathVariable(value = "num1") String num1, @PathVariable(value = "num2") String num2)
+			throws Exception {
+
+		if (!isNumeric(num1) || !isNumeric(num2)) {
+			throw new Exception();
+		}
+		return convertToDouble(num1) + convertToDouble(num2);
+	}
+
+	private Double convertToDouble(String strNum) {
+		if (strNum == null) return 0D;
+		
+		String number = strNum.replaceAll(",", ".");
+		if (isNumeric(number)) return Double.parseDouble(number);
+
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNum) {
+		if (strNum == null) return false;
+		String number = strNum.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
 }
