c746ab0bacee9a6d245184d61831851e669b921d
Alex Sandro <alex.sandro.1199@gmail.com>
2023-03-02 17:00:27 -0300

    Refactoring PersonController

--- 08_SpringDataJPA/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/controllers/PersonController.java
+++ 08_SpringDataJPA/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/controllers/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 
-	@GetMapping()
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findAll() {
 		return service.findAll();
 	}
@@ -33,18 +34,21 @@ public class PersonController {
 		return service.findById(id);
 	}
 
-	@PostMapping()
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person create(@RequestBody Person person) {
 		return service.create(person);
 	}
 
-	@PutMapping()
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person update(@RequestBody Person person) {
 		return service.update(person);
 	}
 
-	@DeleteMapping()
-	public void delete(@PathVariable(value = "id") Long id) {
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
 		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- 08_SpringDataJPA/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/repositories/PersonRepository.java
+++ 08_SpringDataJPA/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/repositories/PersonRepository.java
@@ -1,11 +1,9 @@
 package com.alex.restwithspringbootandjava.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.alex.restwithspringbootandjava.model.Person;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
 }
