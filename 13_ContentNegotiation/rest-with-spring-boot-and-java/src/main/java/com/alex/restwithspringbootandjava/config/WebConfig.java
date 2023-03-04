e704350d5ba9a7fe551f6776b5d871712a295684
Alex Sandro <alex.sandro.1199@gmail.com>
2023-03-04 10:06:07 -0300

    Serialization with YAML

--- 13_ContentNegotiation/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/config/WebConfig.java
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/config/WebConfig.java
@@ -1,12 +1,19 @@
 package com.alex.restwithspringbootandjava.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.alex.restwithspringbootandjava.serialization.converter.YamlJackson2HttpMessageConverter;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -26,6 +33,12 @@ public class WebConfig implements WebMvcConfigurer {
 		.useRegisteredExtensionsOnly(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
 		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);
+		.mediaType("xml", MediaType.APPLICATION_XML)
+		.mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
+	}
+	
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());
 	}
 }
--- 13_ContentNegotiation/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/controllers/PersonController.java
+++ 13_ContentNegotiation/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/controllers/PersonController.java
@@ -3,7 +3,6 @@ package com.alex.restwithspringbootandjava.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.alex.restwithspringbootandjava.data.vo.v1.PersonVO;
 import com.alex.restwithspringbootandjava.services.PersonServices;
+import com.alex.restwithspringbootandjava.util.MediaType;
 
 @RestController
 @RequestMapping("/api/person/v1")
@@ -25,27 +25,27 @@ public class PersonController {
 	private PersonServices service;
 
 	@GetMapping(
-			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
 
 	@GetMapping(value = "/{id}",
-			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
 
 	@PostMapping(
-			consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
-			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}, 
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	public PersonVO create(@RequestBody PersonVO PersonVO) {
 		return service.create(PersonVO);
 	}
 
 	@PutMapping(
-			consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, 
-			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML }, 
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	public PersonVO update(@RequestBody PersonVO PersonVO) {
 		return service.update(PersonVO);
 	}
