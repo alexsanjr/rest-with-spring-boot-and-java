a85f33896f8c0f42cc3bade614568f121ad8919e
Alex Sandro <alex.sandro.1199@gmail.com>
2023-03-15 07:57:38 -0300

    CORS globally

--- 16_CORS/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/config/WebConfig.java
+++ 16_CORS/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/config/WebConfig.java
@@ -2,10 +2,12 @@ package com.alex.restwithspringbootandjava.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.alex.restwithspringbootandjava.serialization.converter.YamlJackson2HttpMessageConverter;
@@ -14,6 +16,26 @@ import com.alex.restwithspringbootandjava.serialization.converter.YamlJackson2Ht
 public class WebConfig implements WebMvcConfigurer {
 	
 	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+	@Value("${cors.originPatterns:default}")
+	private String corsOriginPatterns = "";
+	
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		String[] allowedOrigins = corsOriginPatterns.split(", ");
+		registry.addMapping("/**")
+		//.allowedMethods("GET", "POST", "PUT")
+		.allowedMethods("*")
+		.allowedOrigins(allowedOrigins)
+		.allowCredentials(true);
+	}
+
+
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -35,10 +57,5 @@ public class WebConfig implements WebMvcConfigurer {
 		.mediaType("json", MediaType.APPLICATION_JSON)
 		.mediaType("xml", MediaType.APPLICATION_XML)
 		.mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
-	}
-	
-	@Override
-	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.add(new YamlJackson2HttpMessageConverter());
 	}
 }
--- 16_CORS/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/controllers/PersonController.java
+++ 16_CORS/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/controllers/PersonController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -54,7 +53,7 @@ public class PersonController {
 		return service.findAll();
 	}
 
-	@CrossOrigin(origins = "http://localhost:8080")
+	//@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(value = "/{id}",
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	@Operation(summary = "Finds a Person", description = "Finds a Person",
@@ -72,7 +71,7 @@ public class PersonController {
 		return service.findById(id);
 	}
 
-	@CrossOrigin(origins =  { "http://localhost:8080", "https://localhost:alex.com" })
+	//@CrossOrigin(origins =  { "http://localhost:8080", "https://localhost:alex.com" })
 	@PostMapping(
 			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}, 
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
