225a65a65a27e1c8db3ec760a3e19af8faa643cb
Alex Sandro <alex.sandro.1199@gmail.com>
2023-03-16 10:45:06 -0300

    UserServices

---
+++ 17_Security/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/services/UserServices.java
@@ -0,0 +1,37 @@
+package com.alex.restwithspringbootandjava.services;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.alex.restwithspringbootandjava.model.User;
+import com.alex.restwithspringbootandjava.repositories.UserRepository;
+
+@Service
+public class UserServices implements UserDetailsService {
+
+	private Logger logger = Logger.getLogger(UserServices.class.getName());
+
+	@Autowired
+	UserRepository repository;
+
+	public UserServices(UserRepository repository) {
+		this.repository = repository;
+	}
+
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		logger.info("Finding one user by name " + username + "!");
+		User user = repository.findByUsername(username);
+		if (user != null) {
+			return user;
+		} else {
+			throw new UsernameNotFoundException("Username " + username + " not found!");
+		}
+	}
+}
