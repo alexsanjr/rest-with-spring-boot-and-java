4c10217f6ba0d64d9a79c3743fcb4e2b634f42ea
Alex Sandro <alex.sandro.1199@gmail.com>
2023-03-17 15:14:59 -0300

    jwtTokenProvider implementation

--- 17_Security/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/securityJwt/JwtTokenProvider.java
+++ 17_Security/rest-with-spring-boot-and-java/src/main/java/com/alex/restwithspringbootandjava/securityJwt/JwtTokenProvider.java
@@ -2,17 +2,27 @@ package com.alex.restwithspringbootandjava.securityJwt;
 
 import java.time.Instant;
 import java.util.Base64;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alex.restwithspringbootandjava.data.vo.v1.security.TokenVO;
+import com.alex.restwithspringbootandjava.exceptions.InvalidJwtAuthenticationException;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Service
 public class JwtTokenProvider {
@@ -38,17 +48,72 @@ public class JwtTokenProvider {
 		Instant now = Instant.now();
 		Instant validity = now.plusMillis(validityInMilliseconds);
 		String accessToken = getAccessToken(username, roles, now, validity);
-		String refreshToken = getAccessToken(username, roles, now);
+		String refreshToken = getRefreshToken(username, roles, now);
+		
 		return new TokenVO(username, true, now, validity, accessToken, refreshToken);
 	}
 
-	private String getAccessToken(String username, List<String> roles, Instant now) {
-		
-		return null;
-	}
 
 	private String getAccessToken(String username, List<String> roles, Instant now, Instant validity) {
-		// TODO Auto-generated method stub
-		return null;
+		String issuerUrl = ServletUriComponentsBuilder
+				.fromCurrentContextPath().build().toUriString();
+		
+		return JWT.create()
+				.withClaim("roles", roles)
+				.withIssuedAt(now)
+				.withExpiresAt(validity)
+				.withSubject(username)
+				.withIssuer(issuerUrl)
+				.sign(algorithm)
+				.strip();
+	}
+	
+	private String getRefreshToken(String username, List<String> roles, Instant now) {
+		Instant validityRefreshToken = now.plusMillis(validityInMilliseconds * 3);
+		
+		return JWT.create()
+				.withClaim("roles", roles)
+				.withIssuedAt(now)
+				.withExpiresAt(validityRefreshToken)
+				.withSubject(username)
+				.sign(algorithm)
+				.strip();
+	}
+	
+	public Authentication getAuthentication(String token) {
+		DecodedJWT decodedJWT = decodedToken(token);
+		UserDetails userDetails = this.userDetailsService
+				.loadUserByUsername(decodedJWT.getSubject());
+		return new UsernamePasswordAuthenticationToken(userDetails,
+				"",userDetails.getAuthorities());
+	}
+
+	private DecodedJWT decodedToken(String token) {
+		Algorithm alg = Algorithm.HMAC256(secretKey.getBytes());
+		JWTVerifier verifier = JWT.require(alg).build();
+		DecodedJWT decodedJWT = verifier.verify(token);
+		return decodedJWT;
+	}
+	
+	public String resolveToken(HttpServletRequest req) {
+		String bearerToken = req.getHeader("Authorization");
+		
+		if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+			return bearerToken.substring("Bearer ".length());
+		}
+		return null;	
+	}
+	
+	public boolean validateToken(String token) {
+		DecodedJWT decodedJWT = decodedToken(token);
+		try {
+			if (decodedJWT.getExpiresAt().before(Date.from(Instant.now()))) {
+				return false;
+			}
+			return true;
+		} catch (Exception e) {
+			throw new InvalidJwtAuthenticationException("Expired or invalid JWT token!");
+		}
+		
 	}
 }
